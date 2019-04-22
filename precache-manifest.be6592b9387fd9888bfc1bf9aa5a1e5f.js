8d0e22e04f068116d6bb769b1bf67f2fe0634acd
Charles Duncan <cduncan98@icloud.com>
2019-04-22 00:31:04 -0700

    Updates

---
+++ precache-manifest.be6592b9387fd9888bfc1bf9aa5a1e5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "6797aab58cce98a61237",
+    "url": "/Portfolio/static/js/main.5929cb0b.chunk.js"
+  },
+  {
+    "revision": "623e5400915711ac529d",
+    "url": "/Portfolio/static/js/2.4a279bf3.chunk.js"
+  },
+  {
+    "revision": "6797aab58cce98a61237",
+    "url": "/Portfolio/static/css/main.5342e1ce.chunk.css"
+  },
+  {
+    "revision": "3068abe465e7fe63c8cd411a3ee8baf5",
+    "url": "/Portfolio/index.html"
+  }
+];
